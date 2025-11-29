1d4732c6cc8184284a446bb6092553a005357fbc
shifasherin7078 <shiff553@gmail.com>
2025-11-29 10:46:05 +0530

    lambda.py

---
+++ lambda.py
@@ -0,0 +1,6 @@
+square = lambda a: a*a
+rectangle = lambda l, b: l*b
+triangle = lambda b, h: 0.5*b*h
+print ("area of square:", square (4))
+print ("area of rectangle:", rectangle(4,6)) 
+print ("area of triangle:", triangle (3,8))

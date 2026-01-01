046f70be45149538d8b64b3fcaf9f936ae02e0e8
shifanasherinn <shiff553@gmail.com>
2026-01-01 20:55:10 +0530

    Delete replace.py

--- replace.py
+++
@@ -1,8 +0,0 @@
-numbers=list(map(int,input("enter number:").split()))
-result=[]
-for num in numbers:
-    if num>100:
-        result.append('over')
-    else:
-        result.append(num)
-print(result)

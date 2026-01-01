43f3354bb388a2a8f48d42f75f372946f1144bfa
shifanasherinn <shiff553@gmail.com>
2026-01-01 20:51:17 +0530

    Delete squarenumber.py

--- squarenumber.py
+++
@@ -1,7 +0,0 @@
-result = []
-for num in range(1000, 10000):
-    if int(num**0.5)**2 == num:
-        digits = str(num)
-        if all(int(d)%2==0 for d in digits):
-            result.append(num)
-print(result)

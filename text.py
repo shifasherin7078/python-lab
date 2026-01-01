817b5667bc160cfc406cbee0f3e32849fa7d9955
shifanasherinn <shiff553@gmail.com>
2026-01-01 20:31:07 +0530

    Delete text.py

--- text.py
+++
@@ -1,9 +0,0 @@
-text=input("enter text:")
-words=text.split()
-word_count={}
-for w in words:
-    if w in word_count:
-        word_count[w]+=1
-    else:
-        word_count[w]=1
-print("word occuerence:",word_count)            

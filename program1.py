e3793ef46b6f6da8d927d5aeeca708671126875b
shifanasherinn <shiff553@gmail.com>
2026-01-02 18:38:39 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,14 +1,6 @@
-import datetime
-
-final_year = int(input("Enter the final year: "))
-
-current_year = datetime.datetime.now().year
-
-print("Leap years from", current_year, "to", final_year, ":")
-
-
-for year in range(current_year, final_year + 1):
-    # Step 6: Check if the year is a leap year
-    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-        # Step 7: Print the leap year
+current_year=2025
+final_year=int(input("enter the final year:"))
+print("leap years are")
+for year in range(current_year,final_year+1):
+    if(year%400==0)or(year%4==0 and year%100!=0):
         print(year)
